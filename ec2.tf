6bd84fc8fd26b6efa8cec6c44d76a22020694a6b
JD <jdbaez@silk.es>
2023-02-05 16:00:31 +0100

    feat: add terraform files

---
+++ ec2.tf
@@ -0,0 +1,40 @@
+data "aws_ami" "app_ami" {
+  most_recent = true
+  owners = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm*"]
+  }
+}
+
+resource "aws_instance" "hello-world" {
+   ami = data.aws_ami.app_ami.id
+   instance_type = var.instance_type
+   key_name = "terraform"
+   vpc_security_group_ids = [ aws_security_group.allow_access.id ]
+
+   connection {
+   type     = "ssh"
+   user     = "ec2-user"
+   private_key = file("./key_pair/terraform.pem")
+   host     = self.public_ip
+    }
+
+ provisioner "remote-exec" {
+   inline = [
+     "sudo amazon-linux-extras install -y nginx1",
+     "sudo systemctl start nginx",
+     "sudo chmod 0777 /usr/share/nginx/html/index.html"
+     ]
+ }
+ provisioner "file" {
+  source = "./nginx/html/hello_mel.html"
+  destination = "/usr/share/nginx/html/index.html"
+ }
+}
+
+output "instance_details"{
+    description = "DNS to Access"
+    value = aws_instance.hello-world.public_ip
+}
---
+++ security_group.tf
@@ -0,0 +1,27 @@
+resource "aws_security_group" "allow_access" {
+  name        = "allow_access"
+  description = "Allow inbound traffic"
+
+dynamic "ingress" {
+    for_each = var.inbound_port
+    iterator = port
+    content {
+      from_port   = port.value
+      to_port     = port.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+  
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_access"
+  }
+}
